02038d11770e8a71e2e4b0e51d24e3d71affab9f
sermsak <tu.sermsak@gmail.com>
2024-10-19 14:42:32 +0700

    feat : ex1-4

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,14 @@
 //Exercise #1: Variable Creation
 //Start coding here
-
+let userName = "Karter";
+let userSurname = "Anderson";
+let address = "54/24 Gp 5 BangKruay-Sainoi Bang Kruay Bang Kruai"
+const shopNAME = "Chef's Table by Chef James"
+const costTotal = 15000;
+const onlineShop = 1500;
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(097) 878-53292400 Country Rd #4Meeker, Colorado(CO), 81641";
+costTotal = 5000;
+onlineShop = 1600;
+
